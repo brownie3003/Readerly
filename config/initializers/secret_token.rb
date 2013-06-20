bd8a95a4ba2c9e633d9ec2c37e6db69fb57c4d31
brownie3003 <brownie3003@gmail.com>
2013-06-20 01:11:43 +0100

    Zahid's mess

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Readerly::Application.config.secret_key_base = 'f5e0969c9a9401b97dd71425634e19dc8879d0e722439d41e4b73b323da41eee537d8fd5b019897f38a46eca80af8f821362340461d4c218ef82396d1981648d'
