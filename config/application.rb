d59e56f474d605d22ddb26a0cdbfa38f310fbb25
brownie3003 <brownie3003@gmail.com>
2013-06-30 23:53:26 +0100

    Heroku failing because action missing for my books model dummy

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,5 @@ module Readerly
     # config.i18n.default_locale = :de
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.initialize_on_precompile = false
   end
 end
