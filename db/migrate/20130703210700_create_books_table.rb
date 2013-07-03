e0eaf5c19e356ccd839722f063dd6f1376d6005b
brownie3003 <brownie3003@gmail.com>
2013-07-03 22:09:57 +0100

    added migrations

---
+++ db/migrate/20130703210700_create_books_table.rb
@@ -0,0 +1,12 @@
+class CreateBooksTable < ActiveRecord::Migration
+  def change
+  	create_table :books do |t|
+    	t.string :title
+    	t.string :author
+    	t.text :synopsis
+    	t.text :chapters
+
+    	t.timestamps
+    end
+  end
+end
